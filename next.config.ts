f2ce6ee5d9f511b832561362d7ef45a7009a7a29
pedro henrique <dnlsniperonly2@gmail.com>
2025-08-28 14:27:55 +0000

    por algum motivo o npm run build ta quebrandoo c´dogio, invetigue e solu

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
