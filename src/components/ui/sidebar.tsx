fa299f146917f5f7654a245eee66c5f8ba4efd68
pedro henrique <dnlsniperonly2@gmail.com>
2025-09-02 18:25:25 +0000

    3:22:58 PM:    Linting and checking validity of types ... 3:23:02 PM: Fa

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -430,12 +430,10 @@ SidebarGroup.displayName = "SidebarGroup"
 
 const SidebarGroupLabel = React.forwardRef<
   HTMLDivElement,
-  React.ComponentProps<"div"> & { asChild?: boolean }
->(({ className, asChild = false, ...props }, ref: React.Ref<HTMLDivElement>) => {
-  const Comp = asChild ? Slot : "div";
-
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
   return (
-    <Comp
+    <div
       data-sidebar="group-label"
       className={cn(
         "duration-200 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opa] ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
